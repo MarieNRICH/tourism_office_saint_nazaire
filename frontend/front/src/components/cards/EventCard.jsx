855f15f016c894db842ab92180dd09d22868c81e
myrdinaptaliesen <nicog.dev@gmail.com>
2024-02-22 11:13:25 +0100

    Modifs frontend

--- frontend/front/src/components/cards/EventCard.jsx
+++ frontend/front/src/components/cards/EventCard.jsx
@@ -1,57 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { BASE_URL } from '../../config/config';
-import { Link } from 'react-router-dom';
+
 import Events from '../../pages/events/Events';
 
 
 
-function EventCard({ props }) {
-    const [events, setEvents] = useState({});
-    const [eventPlaces, setEventPlaces] = useState({});
-    const [photoPlace, setPhotoPlace] = useState({});
-    const [places, setPlaces] = useState({});
-    const place_id = props;
-
-
-    useEffect(() => {
-        console.log(place_id);
-        displayEvents();
-        displayPlaces();
-    }, []);
-
-    const displayEvents = async () => {
-        await axios.get(`${BASE_URL}/Events`).then((res) => {
-            setEvents(res.data);
-        });
-    };
-
-    const displayPlaces = async () => {
-        await axios.get(`${BASE_URL}/Places`).then((res) => {
-            setEvents(res.data);
-        });
-    };
-
-
+function EventCard({ event }) {
+    
     return (
 
         <Card style={{ width: "100%" }} className='my-2' >
-            <Card.Img variant="top" src={`${BASE_URL}/${places.photoPlace}`} alt="Event Image" />
-            {events.map((event) => (
+            <Card.Img variant="top" src={`${BASE_URL}/${event.image}`} alt="Event Image" />
+            
                 <Card.Body>
                     <Card key={event.id}>
                         <Card.Title>{event.title}</Card.Title>
                         <Card.Text>
                             {event.detail}
                         </Card.Text>
-                        <Button variant="primary" onClick={() => {
-                            displayEvents(event.id);
-                        }}>Go somewhere</Button>
+                        
+                        <Link to={`/events/${event.id}`} className='btn btn-success me-2'>En savoir + </Link>
                     </Card>
                 </Card.Body>
-            ))}
+           
         </Card>
     );
 }
--- frontend/front/src/pages/home.jsx
+++ frontend/front/src/pages/home.jsx
@@ -1,52 +1,53 @@
-import React, { Component, useEffect, useState } from 'react';
-import axios from 'axios';
-import Menu from '../components/Menu';
-import Logo from '../components/Logo';
-import Video from '../components/Video';
-import { BASE_URL } from '../config/config';
+import React, { Component, useEffect, useState } from "react";
+import axios from "axios";
+import Menu from "../components/Menu";
+import Logo from "../components/Logo";
+import Video from "../components/Video";
+import { BASE_URL } from "../config/config";
 // import EventCard from '../components/cards/EventCard';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/esm/Container';
-import EventCard from '../components/cards/EventCard';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/esm/Container";
+import EventCard from "../components/cards/EventCard";
 
 const Home = () => {
+  const [events, setEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    displayEvents();
+  }, []);
 
-    const [places, setPlaces] = useState([]);
+  const displayEvents = async () => {
+    await axios.get(`${BASE_URL}/Events`).then((res) => {
+      setEvents(res.data);
+      setIsLoading(true);
+    });
+  };
 
-    useEffect(() => {
-        displayPlaces();
-    }, []);
+  console.log(events);
 
-    const displayPlaces = async () => {
-        await axios.get(`${BASE_URL}/Places`).then((res) => {
-            setPlaces(res.data);
-            console.log(res.data);
-        });
-    };
-
-
-    return (
-        <React.Fragment>
-            <div>
-                <Logo />
-                <Menu />
-            </div>
-            {/* <div>
+  return (
+    <React.Fragment>
+      <div>
+        <Logo />
+        <Menu />
+      </div>
+      {/* <div>
                 <Video />
             </div> */}
-            <section>
+      <section>
+        <h1>Toto</h1>
+        {isLoading
+          ? events.map((event) => (
+              <div key={event.id} xs={12} sm={6} md={3}>
                 <h1>Toto</h1>
-                {places.map((place) => {
-                    <div xs={12} sm={6} md={3}>
-                        <h1>Toto</h1>
-                        <EventCard place_id={place.id} />
-                    </div>
-                }
-                )}
 
-            </section>
-        </React.Fragment>
-    )
-}
+                 <EventCard event={event} />
+              </div>
+            ))
+          : null}
+      </section>
+    </React.Fragment>
+  );
+};
 export default Home;
